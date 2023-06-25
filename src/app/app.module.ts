44d1602f75b030b9f38177b77d1027a40631bd19
Mukesh A <70726140+mukeshakula418@users.noreply.github.com>
2023-06-25 18:45:06 -0500

    initial working application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { RatingModule } from "ngx-bootstrap/rating";
+import { ProductModule } from "./products/product.module";
+import { RouterModule } from "@angular/router";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AddProductComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BsDatepickerModule.forRoot(),
+    RatingModule.forRoot(),
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'add-product', component: AddProductComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
